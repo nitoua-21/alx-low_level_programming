6835641bdf744f876b210d1247ea7153e423a75a
nitoua-21 <nitoua.dev@gmail.com>
2022-09-09 01:41:49 +0100

    Add: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: prints exactly:
+ *"Programming is like building a multilingual puzzle,
+ *followed by a new line.
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sentence[70] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(sentence);
+
+	return (0);
+}
