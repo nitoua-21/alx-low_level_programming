672bd9764df78cbcbc55ebf51dccdf5b274bef99
nitoua-21 <nitoua.dev@gmail.com>
2022-09-09 01:44:20 +0100

    Edit: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char sentence[70] = "\"Programming is like building a multilingual puzzle\n";
+	char sentence[70] = "\"Programming is like building a multilingual puzzle";
 
 	puts(sentence);
 
