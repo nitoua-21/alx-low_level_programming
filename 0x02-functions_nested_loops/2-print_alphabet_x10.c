78660600d2eac10a86b980e70bf64cbd1aed5111
nitoua-21 <nitoua.dev@gmail.com>
2022-09-13 23:54:13 +0100

    Update: 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,17 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char c;
 
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
 }
