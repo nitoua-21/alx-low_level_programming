fb66db0af9e94d19be45e7ff772ec3fc0f088ff2
nitoua-21 <nitoua.dev@gmail.com>
2022-09-14 03:23:55 +0100

    Refactor: Betty style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			product = i*j;
+			product = i * j;
 
 			if (product < 10)
 				_putchar(product + '0');
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
-			
+
 			if (j < 9 && (product + i <= 9))
 			{
 				_putchar(',');
