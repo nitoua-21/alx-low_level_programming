96f4bc2b355dacb31476f119907c92892d88766b
nitoua-21 <nitoua.dev@gmail.com>
2022-09-15 01:06:50 +0100

    Fix: exlude negative value

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,42 +11,41 @@ void print_times_table(int n)
 {
 	int i, j, k, l, product, ratio, num_digits;
 
-	for (i = 0; i <= n; i++)
+	if (!(n < 0 || n > 15))
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-			ratio = product / 10;
-			l = 1;
-
-			/* Counts number of digits of n */
-			while (ratio)
+			for (j = 0; j <= n; j++)
 			{
-				ratio /= 10;
-				l *= 10;
+				product = i * j;
+				ratio = product / 10;
+				l = 1;
+				/* Counts number of digits of n */
+				while (ratio)
+				{
+					ratio /= 10;
+					l *= 10;
+				}
+
+				for (k = l; k > 0; k /= 10)
+					_putchar((product / k) % 10 + '0');
+
+				num_digits = 1;
+				ratio = (product + i) / 10;
+				/*Get number of digits*/
+				while (ratio)
+				{
+					num_digits++;
+					ratio /= 10;
+				}
+				if (j < n)
+				{
+					_putchar(',');
+					for (k = 0; k < 4 - num_digits; k++)
+						_putchar(' ');
+				}
 			}
-
-			for (k = l; k > 0; k /= 10)
-				_putchar((product / k) % 10 + '0');
-
-			num_digits = 1;
-			ratio = (product + i) / 10;
-
-			/*Get number of digits*/
-			while (ratio)
-			{
-				num_digits++;
-				ratio /= 10;
-			}
-
-			if (j < n)
-			{
-				_putchar(',');
-				for (k = 0; k < 4 - num_digits; k++)
-					_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-
 }
