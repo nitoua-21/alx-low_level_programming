e6571e8e39a1ab8fd6689e67d6ddbead3d108915
nitoua-21 <nitoua.dev@gmail.com>
2022-09-16 23:12:22 +0100

    Add: function _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase character
+ *@c: caracter to be checked
+ *
+ *Return: 1 if c is uppercase,
+ *0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 90)
+		return (1);
+	return (0);
+}
