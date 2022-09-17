6c9014307a6f66e56fbaddeb53cac819d6d1da3b
nitoua-21 <nitoua.dev@gmail.com>
2022-09-17 03:53:19 +0100

    Add: print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+/**
+ *digit_count - Counts the number of digits of an integer
+ *@n: the number to count digits
+ *
+ *Return: count of digits
+ */
+int digit_count(int n)
+{
+	int count = 0;
+
+	do {
+		n /= 10;
+		count++;
+	} while (n != 0);
+
+	return (count);
+}
+
+/**
+ *print_number - prints an integer on the stdout
+ *@n: integer to be printed
+ */
+void print_number(int n)
+{
+	int i, j, count, divider;
+
+	count = digit_count(n);
+	divider = 1;
+	/*Evaluate 10 to the power count - 1*/
+	for (i = 1; i < count; i++)
+		divider *= 10;
+	if (n < 0)
+		_putchar('-');
+	for (j = divider; j > 0; j /= 10)
+		_putchar(n >= 0 ? (n / j) % 10 + '0' : (-1 * n / j) % 10 + '0');
+}
+
