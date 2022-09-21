38e1634466fd3f20dc6aba93cd7f0a1d5e3db1a4
Normich ITOUA <nitoua.dev@gmail.com>
2022-09-21 18:45:02 +0000

    Add: function string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ *string_toupper - changes all lowercase letters of a string to upppercase
+ *@str: string to be changed
+ *
+ *Return: uppercase string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+
+	return (str);
+}
