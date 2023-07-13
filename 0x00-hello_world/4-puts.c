1926e8b313087f2132bca6797cce834514c89aae
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-13 03:17:37 -0700

    Add Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ *main -  Entry point
+ *
+ *Description: prints exactly:
+ *"Programming is like building a multilingual puzzle"
+ *followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sentence[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(sentence);
+
+	return (0);
+}
