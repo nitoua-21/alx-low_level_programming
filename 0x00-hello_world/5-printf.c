ab8b82302c967f0c6a24372a5f231491b2c4aa69
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-13 03:33:43 -0700

    Add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ *main - Enty point
+ *
+ *Description: prints exactly:
+ *"with proper grammar, but the outcome is a piece of art,"
+ *followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
