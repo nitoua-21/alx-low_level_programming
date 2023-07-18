395d2ca353e28b87a417d665bb28c2750b81dcd6
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 06:27:58 -0700

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day
+ *from 00:00 to 23:59
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			/* i%10 for firt digit & i%10 for last digit  */
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
+
