46ec623a5c2a1940a9e1d9f1a624be5c19bc9a2b
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 14:44:38 -0700

    Add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: computes and prints the sum of multiples of 3 and 5.
+ *
+ *Return: 0 Always (Success).
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 3; i < 1024 ; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d", sum);
+
+	return (0);
+}
