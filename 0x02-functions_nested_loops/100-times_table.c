829ab68844bc093ad2c3ca6adfc4aa0f86f9609f
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 14:17:00 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,9 +3,7 @@
  *print_times_table - prints n times table,
  *staring with 0.
  *@n: number of times
- *
  */
-
 
 void print_times_table(int n)
 {
@@ -20,29 +18,33 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				prod = i * j;
-
-				/*print product*/
-				if (prod < 10)
+				if (j == 0)
 				{
 					_putchar(prod + '0');
 				}
-				else if (prod < 100)
-				{
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
-				}
 				else
-				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-					_putchar((prod % 10) + '0');
-
-				}
-				if (j < n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
+					if (prod < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(prod + '0');
+					}
+					else if (prod < 100)
+					{
+						_putchar(' ');
+						_putchar((prod / 10) + '0');
+						_putchar((prod % 10) + '0');
+					}
+					else
+					{
+						_putchar((prod / 100) + '0');
+						_putchar((prod / 10) % 10 + '0');
+						_putchar((prod % 10) + '0');
+
+					}
 				}
 			}
 			_putchar('\n');
