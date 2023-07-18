089b26c25fcc3817bc9e2ed318e5e4de0eaeaccd
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 16:09:42 -0700

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Decsription:  prints the sum of the even-valued terms of Fibonacci numbers
+ *
+ *Return: 0 Always (Success).
+ */
+
+int main(void)
+{
+	int i;
+	long int a;
+	long int b;
+	long int sum;
+
+	a = 1;
+	b = 2;
+	sum = 0;
+
+
+	while (b <= 4000000)
+	{
+		a += b;
+		b += a;
+
+		if ((a % 2) == 0)
+			sum += a;
+		if ((b % 2) == 0)
+			sum += b;
+	}
+
+	printf("%li\n", sum);
+
+	return (0);
+
+}
