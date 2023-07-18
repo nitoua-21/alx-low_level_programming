fe4340e3dbd4402f4e086fd3b985d781c1980f2c
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 06:23:42 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		last_digit = (unsigned int) n % 10;
-
 	last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
 
 	_putchar(last_digit + '0');
 
