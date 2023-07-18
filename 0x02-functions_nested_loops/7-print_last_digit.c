3f4235eebd42b4f1548b4ab777ba3ae33f4c719c
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 06:07:07 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,11 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
-
 	if (n < 0)
 		n *= -1;
-	last_digit = n % 10;
 
-	_putchar(last_digit + '0');
+	_putchar((n % 10) + '0');
 
-	return (last_digit);
+	return (n % 10);
 }
 
