a6db560287a8ae6d081b735efb698e10ec7f76bb
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-18 15:38:32 -0700

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Decsription: prints the first 50 Fibonacci numbers
+ *
+ *Return: 0 Always (Success).
+ */
+
+int main(void)
+{
+	int i;
+	long long int a;
+	long long int b;
+
+	a = 1;
+	b = 2;
+
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%li, %li", a, b);
+		a += b;
+		b += a;
+
+		if (i < 49)
+			printf(", ");
+
+	}
+
+}
