51dab2e95218cfd77d6df40df2d272f1876fe341
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-20 15:41:49 -0700

    fix 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,15 @@ void print_number(int n)
 	int count;
 	double num;
 
+	num = n;
+	count = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num *= -1;
 	}
 
-	num = n;
-	count = 0;
 
 	do {
 		num /= 10;
@@ -24,7 +25,7 @@ void print_number(int n)
 	} while (num > 1);
 
 	do {
-		num -= (int) num;
+		num -= (unsigned int) num;
 		num = num * 10;
 		_putchar('0' + (int) num);
 		count--;
