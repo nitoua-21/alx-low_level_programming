30302ca86315e156866d097afa19844fa6491224
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-24 08:38:23 -0700

    Add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of string.
+ *@s: string variable
+ *
+ *Return: Length of the string s
+ *
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ *puts_half - prints half of a string
+ *@str : string to be printed
+ *
+ */
+void puts_half(char *str)
+{
+	int i, length;
+
+	/*Get length of the str*/
+	length = _strlen(str);
+
+	i = length % 2 == 0 ? length / 2 : (length - 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+
+	}
+	_putchar('\n');
+}
+
