2cde8e7bab6318211ae2593f90e4e73d268206fa
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-24 19:18:24 -0700

    Add 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random valid passwords for the program 101-crackme.
+ * at https://github.com/alx-tools/0x04.c.
+ *
+ *Return: 0 Always (Success);
+ */
+int main(void)
+{
+	srand(time(NULL));
+	const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int password_length = 8;
+	int random_index;
+
+	for (int i = 0; i < password_length; i++)
+	{
+		random_index = rand() % (sizeof(valid_chars) - 1);
+		printf("%c", valid_chars[random_index]);
+	}
+	printf("\n");
+	return (0);
+}
