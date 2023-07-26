62eb63ce0af8d5bb784f7e173e7902e79e725200
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-26 06:19:13 -0700

    Add 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+/**
+ *rot13 - encodes a string using rot13.
+ *@str: string to be encoded.
+ *
+ *Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
