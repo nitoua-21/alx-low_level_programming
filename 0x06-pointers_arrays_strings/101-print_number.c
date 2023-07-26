1fccf401c1f948cf1cae44850318faf2250bec72
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-26 09:25:53 -0700

    Add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer on the stdout
+ *@n: integer to be printed
+ */
+void print_number(int n)
+{
+	int count;
+	double num;
+
+	count = 0;
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+
+
+	do {
+		num /= 10;
+		count++;
+	} while (num >= 1);
+
+	do {
+		num -= (unsigned int) num;
+		num = num * 10;
+		_putchar('0' + (unsigned int) num);
+		count--;
+	} while (count > 0);
+
+}
+
+
