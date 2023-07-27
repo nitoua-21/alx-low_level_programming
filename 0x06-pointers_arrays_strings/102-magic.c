d69eef430ab3910d6186fe02581991639febb778
Normich ITOUA <nitoua.dev@gmail.com>
2023-07-27 01:41:59 -0700

    Add 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,8 +17,6 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
-  for (n = 0; n < 80; n++)
-	  printf("%d\n", *(p -  n));
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
