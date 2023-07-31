eebebce003787f998ed46e29553ae1f3512ef93c
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-01 00:05:26 +0100

    Fix 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
  *_memcpy - copies memory area.
  *@dest: memory area to copy to
  *@src: memory area to cpy from
- *n: bytes to copy
+ *@n: bytes to copy
  *
  *Return: pointer to dest.
  */
@@ -10,11 +10,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	if (*src == '\0')
-		return (dest);
-
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
+
 
 	return (dest);
 }
