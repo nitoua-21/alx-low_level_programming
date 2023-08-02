874e269371ae7aab0d5e3573c889ab8c021852e8
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-02 09:26:57 -0700

    Add Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,20 @@
 /**
+ *is_divisible - checks for divisibility of n,
+ *by all numbers up to n / 2 starting at 3.
+ *@n: number,
+ *@div: divider
+ *Return: 0 if n is divisible, 1 otherwise.
+ */
+int is_divisible(int n, int div)
+{
+	if (div >  n / 2)
+		return (1);
+	else if (n % div == 0)
+		return (0);
+	return (is_divisible(n, div + 1));
+}
+
+/**
  *is_prime_number - Checks for prime number
  *@n: number
  *
@@ -7,7 +23,9 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1 || n == 2)
+	if (n == 0 || n == 1 || n == -1)
+		return (0);
+	else if (n == 2)
 		return (1);
-	
+	return (is_divisible(n, 2));
 }
