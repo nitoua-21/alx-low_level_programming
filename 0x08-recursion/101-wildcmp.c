568f475683f76e0872c7ab52c0315169cbf124e6
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-03 00:08:16 +0100

    Add Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+/**
+ *wildcmp - compares two strings.
+ *@s1: first string
+ *@s2: second string.
+ *
+ *Return: 1 if s1 and s2 are identical,
+ *0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2 + 1))
+				return (1);
+			s1++;
+
+		}
+		return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
