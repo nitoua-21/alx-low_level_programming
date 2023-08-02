c2bdb0333dc3974ba41b0a9189f6a3eefba517d9
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-02 06:26:47 -0700

    Add Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+/**
+ *_pow_recursion - returns the value of x raised to the power of y.
+ *@x: number
+ *@y: power
+ *
+ *Return: -1 if y is lower than 0,
+ *x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
