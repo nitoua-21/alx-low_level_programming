1c5b13018a621cadc48ef1f7979cb6acaede393f
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-03 00:54:34 +0100

    Fix Task 8: remove while loop

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,14 +14,12 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-
-		}
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 != '\0' &&  wildcmp(s1 + 1, s2))
+			return (1);
 		return (wildcmp(s1, s2 + 1));
+
 	}
 	return (0);
 }
