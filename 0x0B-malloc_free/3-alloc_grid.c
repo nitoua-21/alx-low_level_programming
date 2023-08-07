616e5dd588cc2564e44e7c9f132d2d4b8025c615
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-07 10:42:03 -0700

    Add Task 3 Draft

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+/**
+ *alloc_grid - create a 2 dimensional array of integers.
+ *@width: width
+ *@height: height
+ *
+ *Return: pointer to array.
+ */
+int **alloc_grid(int width, int height)
+{
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+}
