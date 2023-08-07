5b1efd7e0c3555834d9bf8cb308911c398b03507
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-07 10:01:07 -0700

    Fix: handle NULL inputs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,16 +24,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len, i = 0;
 	char *str;
 
-	len = _strlen(s1) + _strlen(s2);
+	len = (s1 == NULL ? 0 : _strlen(s1)) + (s2 == NULL ? 0 : _strlen(s2));
 
 	str = malloc(sizeof(char) * len + 1);
 
 	if (str == NULL)
 		return (NULL);
 
-	while ((str[i] = *s1) != '\0')
+	while (s1 != NULL && (str[i] = *s1) != '\0')
 		i++, s1++;
-	while ((str[i] = *s2) != '\0')
+	while (s2 != NULL && (str[i] = *s2) != '\0')
 		i++, s2++;
 	str[i] = '\0';
 
