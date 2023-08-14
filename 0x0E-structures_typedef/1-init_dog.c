4568d47daaa43d5d6815557be7e213472cf18348
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-14 03:57:26 -0700

    Fix task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "dog.h"
 /**
  *init_dog - Initializes a variable of type struct dog
@@ -10,7 +9,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (!d)
 		return;
 
