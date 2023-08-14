b5fa533134c7360e206b1b59a01a4d3dd89c18a3
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-14 04:45:09 -0700

    Add task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  *struct dog - Represents information about a dog.
  *@name: name
  *@age: age;
@@ -18,5 +23,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
