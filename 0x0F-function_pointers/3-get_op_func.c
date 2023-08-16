a836a9401034e5217a8c6fa4116615a62f6e08f0
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-16 10:12:29 -0700

    Fix Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,10 +21,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i++].op != NULL)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
