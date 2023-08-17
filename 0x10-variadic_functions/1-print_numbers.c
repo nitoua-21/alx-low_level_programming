005fd68cbf8bac8e55e5964de29218b89a7186bc
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-17 06:51:47 -0700

    Fix: always print newline

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,16 +12,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n == 0)
-		return;
-	va_start(args, n);
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", va_arg(args, int));
-		if (!separator || i == n - 1)
-			continue;
-		printf("%s", separator);
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (!separator || i == n - 1)
+				continue;
+			printf("%s", separator);
+		}
+		va_end(args);
 	}
-	va_end(args);
 	printf("\n");
 }
