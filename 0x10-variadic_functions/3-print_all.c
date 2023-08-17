33b461663ed9ca5ea66759d6244ee3b05dbad959
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-17 10:01:28 -0700

    Add Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints a character
+ * @args: args
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an integer
+ * @args: args
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: args
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+
+/**
+ * print_string - prints a string
+ * @args: args
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str)
+		printf("%s", str);
+	else
+		printf("(nil)");
+}
+
+/**
+ * print_all -  prints anything.
+ * @format: list of types of arguments.
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *sep = "";
+	int i, j = 0;
+
+	dtype ops[] = {{'c', print_char},
+			{'i', print_int},
+			{'f', print_float},
+			{'s', print_string},
+			{'\0', NULL}};
+
+	va_start(args, format);
+	while (format && format[j])
+	{
+		i = 0;
+		while (ops[i].c)
+		{
+			if (ops[i].c == format[j])
+			{
+				printf("%s", sep);
+				ops[i].func(args);
+				sep = ", ";
+			}
+			i++;
+		}
+		j++;
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct dt - structure
+ * @c: char representing data type
+ * @func: function pointer
+ */
+typedef struct dt
+{
+	char c;
+	void (*func)(va_list);
+} dtype;
 
 #endif
