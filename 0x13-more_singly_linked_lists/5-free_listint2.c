f3d3e3b50a814be459f11c969f5af4cd9932e30d
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-28 09:58:00 -0700

    Add Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t lis and sets head to NULL
+ * @head: head node
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *tmp = current;
+
+		current = current->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
