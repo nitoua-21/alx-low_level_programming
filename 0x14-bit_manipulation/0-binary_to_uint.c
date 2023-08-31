23370ec8b7dce0bbc2d03f6579c5315e637880a5
Normich ITOUA <nitoua.dev@gmail.com>
2023-08-31 09:26:19 -0700

    Add Task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,4 @@
 #include <stdio.h>
-/**
- * _pow - Evaluates x raised to power y.
- * @x: x
- * @y: y
- * Return: x raised to power y
- */
-unsigned int _pow(unsigned int x, unsigned int y)
-{
-	if (y == 0)
-		return (1);
-	return (x * _pow(x, y - 1));
-}
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
@@ -22,7 +10,7 @@ unsigned int _pow(unsigned int x, unsigned int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, j = 0, n = 0;
+	unsigned int i = 0, j = 0, n = 0, val;
 
 	if (b == NULL || *b == '\0')
 		return (0);
@@ -35,7 +23,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' &&  b[i] != '1')
 			return (0);
-		n += (b[i] - 48) * _pow(2, j--);
+		val = b[i] - 48;
+		n += val << j--;
 
 	}
 
