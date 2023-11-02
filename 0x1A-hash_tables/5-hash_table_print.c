0e95ac0dcf153fd01f1bbbd73ea744c47c5b374f
Normich <nitoua.dev@gmail.com>
2023-11-02 15:53:24 -0700

    Add Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,6 +8,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
 	unsigned long i;
+	char comma = "";
 
 	if (ht == NULL || ht->array == NULL)
 		return;
@@ -19,10 +20,9 @@ void hash_table_print(const hash_table_t *ht)
 
 		while (current != NULL)
 		{
-			printf("'%s': '%s'", current->key, current->value);
+			printf("%s'%s': '%s'", comma, current->key, current->value);
+			comma = ",";
 			current = current->next;
-			if (current != NULL)
-				printf(", ");
 		}
 	}
 	printf("}\n");
