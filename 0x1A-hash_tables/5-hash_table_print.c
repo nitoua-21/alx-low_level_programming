388ccfe9e392416c454fb73bfedb58e6758ea430
Normich <nitoua.dev@gmail.com>
2023-11-02 10:23:55 -0700

    Add Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print the contents of a hash table.
+ * @ht: The hash table to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			if (i < ht->size - 1 || current != NULL)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
