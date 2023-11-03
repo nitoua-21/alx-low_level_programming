6ca95e4db6750d30f94820c87bf6f6627104424b
Normich <nitoua.dev@gmail.com>
2023-11-03 09:48:58 -0700

    Fix Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node, *current;
+	char *val;
 
 	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
+	val = strdup(value);
+	if (!val)
+		return (0);
 
+	current = ht->array[index];
+	while (current && (strcmp(key, current->key) != 0))
+		current = current->next;
+	if (current != NULL)
+	{
+		if (current->value)
+			free(current->value);
+		current->value = val;
+		return (1);
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -29,19 +43,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	current = ht->array[index];
-	while (current && (strcmp(key, current->key) != 0))
-		current = current->next;
-	new_node->next = current;
+	new_node->value = val;
 	ht->array[index] = new_node;
+	new_node->next = current;
 
 	return (1);
 }
