cadd60644c85c24a27b2527f77b3415048c52b26
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-13 00:27:14 +0100

    Fix betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,50 +13,43 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-
 	hash_node_t *new_node = NULL;
-    hash_node_t *current = NULL;
+	hash_node_t *current = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-
-    index = key_index((const unsigned char *)key, ht->size);
-
-    /* Check if key already exists*/
-    current = ht->array[index];
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            /* Key already exists, update value */
-            free(current->value);
-            current->value = strdup(value);
-            if (current->value == NULL)
-                return (0);
-            return (1);
-        }
-    }
-    /* Key does not exist, create new node */
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];/* Check if key already exists*/
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			/* Key already exists, update value */
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+	/* Key does not exist, create new node */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
-	/*Create the new node*/
 	new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-    new_node->next = ht->array[index];
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
