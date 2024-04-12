aa602dfd83e722b5b34eba2d3b58393004b76f65
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-12 23:42:42 +0100

    Update task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_node_t *new_node = NULL;
 
+	if (!*key)
+		return (0);
+
 	new_node = malloc(sizeof(hash_node_t *));
 	if (new_node == NULL)
 		return (0);
