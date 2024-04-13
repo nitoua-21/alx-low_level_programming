365640f0c0f4d0ba90ed8d2108ab5fa1d0ae6682
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-13 02:15:58 +0100

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table.
+*
+* @ht: the hash table
+*
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+
+	unsigned long int count = 0;
+
+	hash_node_t *first = NULL;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		first = ht->array[i];
+		if (first != NULL)
+		{
+			if (++count != 1)
+			{
+				printf(", ");
+			}
+			while (first != NULL)
+			{
+				printf("'%s': '%s'", first->key, first->value);
+				first = first->next;
+			}
+		}
+	}
+	printf("}\n");
+}
