027dddbfc3cfcc6d358e1f763cd63d058d61a2cf
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-13 01:06:28 +0100

    Fix infinite loop on collusion

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,16 +24,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			/* Key already exists, update value */
 			free(current->value);
 			current->value = strdup(value);
 			if (current->value == NULL)
 				return (0);
 			return (1);
 		}
+		current = current->next;
 	}
 	/* Key does not exist, create new node */
-	new_node = malloc(sizeof(hash_node_t *));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
