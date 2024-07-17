bf856d880b487620d21d452233147a80b353abbf
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-17 23:09:32 +0100

    Add Task 9

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,32 +1,32 @@
 #include "search_algos.h"
 
 /**
-* print_range - helper function to print the range being searched
-* @i: index before jump
-* @j: index after jump
-*/
+ * print_range - helper func to print at each check
+ * @i: index before jump
+ * @j: index after jump
+ */
 void print_range(size_t i, size_t j)
 {
 	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
 }
 
 /**
-* print_i - helper function to print index and value at each check
-* @array: array
-* @i: index checked
-*/
+ * print_i - helper func to print idx each check
+ * @array: array
+ * @i: index checked
+ */
 void print_i(int *array, size_t i)
 {
 	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 }
 
 /**
-* jump_search - find square root, jump steps to find match, end linear search
-* @array: given array of ints
-* @size: size of array
-* @value: value to search for
-* Return: index at which value's found
-*/
+ * jump_search - find square root, jump steps to find match, end linear search
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t jumps = sqrt(size);
@@ -35,34 +35,38 @@ int jump_search(int *array, size_t size, int value)
 
 	if (!array)
 		return (-1);
-
-	while (j < size)
+	while (j <= size)
 	{
-		print_i(array, j);
+		if (j != 0)
+			print_i(array, i);
 		if (array[j] >= value)
 		{
 			print_range(i, j);
-			while (i <= j && i < size)
+			while (i <= j)
 			{
 				print_i(array, i);
 				if (array[i] == value)
-					return ((int)i);
+					return (i);
 				i++;
 			}
 			return (-1);
 		}
+		if (j + jumps + jumps > size)
+		{
+			print_i(array, j);
+			print_range(j, j + jumps);
+			while (j < size)
+			{
+				print_i(array, j);
+				if (array[j] == value)
+					return (j);
+				j++;
+			}
+			return (-1);
+
+		}
 		i = j;
 		j += jumps;
 	}
-
-	print_range(i, size - 1);
-	while (i < size)
-	{
-		print_i(array, i);
-		if (array[i] == value)
-			return ((int)i);
-		i++;
-	}
-
 	return (-1);
 }
