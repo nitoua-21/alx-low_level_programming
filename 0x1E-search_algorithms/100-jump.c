3cbea6914d4ed48eeff31176a7bfabec65c53cda
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-17 23:05:57 +0100

    Add task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,40 +1,67 @@
 #include "search_algos.h"
+
 /**
-* jump_search - Searches for a value in a sorted array of integers
-* using the Jump search algorithm
-* @array: Pointer to the first element of the array to search in
-* @size: Number of elements in array
-* @value: Value to search for
-*
-* Return: The first index where value is located, or -1 if value is not present
-* in array or if array is NULL
+* print_range - helper function to print the range being searched
+* @i: index before jump
+* @j: index after jump
+*/
+void print_range(size_t i, size_t j)
+{
+	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
+}
+
+/**
+* print_i - helper function to print index and value at each check
+* @array: array
+* @i: index checked
+*/
+void print_i(int *array, size_t i)
+{
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+}
+
+/**
+* jump_search - find square root, jump steps to find match, end linear search
+* @array: given array of ints
+* @size: size of array
+* @value: value to search for
+* Return: index at which value's found
 */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump, step;
-	size_t prev = 0;
-	size_t i;
+	size_t jumps = sqrt(size);
+	size_t i = 0;
+	size_t j = 0;
 
-	if (array == NULL || size == 0)
+	if (!array)
 		return (-1);
 
-	jump = (size_t)sqrt(size);
-	step = jump;
-
-	while (step < size && array[step] < value)
+	while (j < size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", step, array[step]);
-		prev = step;
-		step += jump;
+		print_i(array, j);
+		if (array[j] >= value)
+		{
+			print_range(i, j);
+			while (i <= j && i < size)
+			{
+				print_i(array, i);
+				if (array[i] == value)
+					return ((int)i);
+				i++;
+			}
+			return (-1);
+		}
+		i = j;
+		j += jumps;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
-
-	for (i = prev; i < size && i <= step; i++)
+	print_range(i, size - 1);
+	while (i < size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		print_i(array, i);
 		if (array[i] == value)
 			return ((int)i);
+		i++;
 	}
 
 	return (-1);
